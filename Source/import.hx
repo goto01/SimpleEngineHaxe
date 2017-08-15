2f4fa2c36188b71f3e31f000b709dc5f2e884e42
Goto01 <kirill.tkachev.ktkach@gmail.com>
2017-08-16 01:16:51 +0300

    Platform dependent

--- Source/import.hx
+++ Source/import.hx
@@ -5,6 +5,18 @@ import openfl.events.*;
 import openfl.Lib;
 import openfl.text.*;
 import openfl.geom.*;
+
+import simpleEngine.*;
 import simpleEngine.core.entitySystem.entities.*;
 import simpleEngine.core.entitySystem.*;
+import simpleEngine.core.resources.*;
+import simpleEngine.core.domain.*;
+import simpleEngine.core.taskSystem.*;
+
 import haxe.*;
+import flash.system.System;
+#if WINDOWS_BUILD
+import cpp.vm.*;
+#else
+import neko.vm.*;
+#end
